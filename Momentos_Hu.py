ac7fb8ad08268ab60e41031d709e86555bad354a
areyci <areyci1997@hotmail.com>
2022-07-26 22:05:26 -0500

    Fin preprocesamiento | Inicios Hu

--- Momentos_Hu.py
+++ Momentos_Hu.py
@@ -1,24 +1,31 @@
 # MOMENTOS DE HU
 import numpy as np
-import cv2
+import cv2 as cv
+import numpy as np
 from skimage import io
 from skimage import filters
-from skimage.color import rgb2gray
-import matplotlib.pyplot as plt
+from math import copysign, log10
 
-# Read image as grayscale image
-im = cv2.imread('Images/A_2.jpg',cv2.IMREAD_GRAYSCALEt
-                t, im = cv2.threshold(im, 110, 255, cv2.THRESH_BINARY)
-                
-#t,im = cv2.threshold(im, 128, 255, cv2.THRESH_BINARY)
+# Leer imagen
+imagen = cv.imread('Images/B.jpg')
+# Eliminación de ruido - Preprocesamiento (2)
+im_ruido = cv.pyrMeanShiftFiltering(imagen,sp=30,sr=50)
+
+# Escala de grises - Preprocesamiento (3)
+im_gris = cv.cvtColor(im_ruido, cv.COLOR_BGR2GRAY)
+
+
+# Binarización | Binarización de Otsu - Segmentación (4)
+ret, im_binaria = cv.threshold(im_gris, 0, 255, cv.THRESH_BINARY)
+
 
 #Calculo de momentos
-moments = cv2.moments(im)
+moments = cv.moments(im_binaria)
 #Calculo de momentos de Hu
-huMoments = cv2.HuMoments(moments)
+huMoments = cv.HuMoments(moments)
 
 # Log scale hu moments 
 for i in range(0,7):
-   huMoments[i] = -1* copysign(1.0, huMoments[i]) * log10(abs(huMoments[i])))
+   huMoments[i] = -1* copysign(1.0, huMoments[i]) * log10(abs(huMoments[i]))
 
 
